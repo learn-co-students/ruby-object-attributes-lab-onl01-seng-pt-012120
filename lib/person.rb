6697e697d51e3cfea3e1e2613f7e74dc20f177a8
Daniel Sun <danielsun12345@gmail.com>
2020-02-03 23:51:19 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(this_person)
+    @name = this_person
+  end 
+  
+  def name
+    @name
+  end
+  
+  def job=(the_job)
+    @job = the_job
+  end 
+  
+  def job
+    @job
+  end
+end 
