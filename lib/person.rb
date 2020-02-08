0f8ced33708424dc2215990c1ba1beeed9cfcaad
Karen Daniela <kdaniimora@gmail.com>
2020-02-08 14:35:57 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+  
+  def alguien=(person_name)
+    @name=person_name
+  end
+  
+  def name 
+    @name 
+end
+ 
+ def name=name
+   @name = name 
+ end 
+ 
+ def job 
+   @job 
+ end 
+ 
+ 
+  def job=(person_job)
+    @job=person_job
+  end
+  
+end 
+    
